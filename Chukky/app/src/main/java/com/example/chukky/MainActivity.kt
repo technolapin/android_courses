015da79ba84ba89d90b6ea2f34f23b0c8f0239c1
technolapin <clement.chomicki@protonmail.com>
2020-04-02 11:09:00 +0200

    clearing disposables when necessary

--- Chukky/app/src/main/java/com/example/chukky/MainActivity.kt
+++ Chukky/app/src/main/java/com/example/chukky/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver.OnScrollChangedListener
-import android.widget.EdgeEffect
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -81,8 +80,7 @@ class MainActivity : AppCompatActivity()
                     ); it.addJoke(j)
                 },
                 onComplete = {
-
-
+                    this.disposables.clear()
                 }
             )
         this.disposables.add(jokeDispo)
